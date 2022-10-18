827d7717f45ef841c7cd73d5c3ac30521e2ff23f
reuben_kl <reubenkl1602@gmail.com>
2022-10-18 22:58:53 +0530

    first clean install

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          React learning
+        </h1>
+        <p> Welcome to the first session</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
