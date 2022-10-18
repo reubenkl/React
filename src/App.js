23cb85c6d4ebaf189a1a46178a35b338e72f7e6a
reuben_kl <reubenkl1602@gmail.com>
2022-10-19 00:10:34 +0530

    done with useState Hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import Employee from "./components/Employee";
+import { useState } from 'react';
+
 
 function App() {
   const allowAccess = true;
+  const [role, setRole] = useState("Developer"); 
   return (
     <div>
       { allowAccess ?
       <>
+      <input 
+            type="text" 
+            onChange={(e) =>{
+                console.log(e.target.value)
+                setRole(e.target.value);
+            }} />
         <Employee name="Reuben" role="Intern"/>
-        <Employee />
-        <Employee />
+        <Employee name="Prafulla" role={role}/>
+        <Employee name="Frankenstein"/>
       </>
       :
       <p> You don't have access</p>
